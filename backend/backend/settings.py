a228db4378413e1fc8e0b069770e39054db474f8
mrTuralkar <turalkarjiten@gmail.com>
2026-06-04 16:18:13 +0530

    deploy code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,8 +70,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
